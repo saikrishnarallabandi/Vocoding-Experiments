682da4abb4662a21fa5634f0bb15fd06aefff1dc
saikrishnarallabandi <srallaba@cs.cmu.edu>
2018-08-02 09:04:09 -0400

    Shifted to SoundFile from scipy for I/O operations

--- segmental_wavenet/02_make_ccoeffs_wav_pm.py
+++ segmental_wavenet/02_make_ccoeffs_wav_pm.py
@@ -1,6 +1,12 @@
 import os
 import numpy as np
 from scipy.io import wavfile as wf
+import soundfile as sf
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
 
 '''
 Read pitchmark file
@@ -9,6 +15,8 @@ Read ccoeffs file and get the ccoeffs.
 Read the wavefile and get corresponding wave.
 Quantize the wave and store <ccoeffs,wav> in numpy array. 
 '''
+
+save_plot = 0
 
 def mulaw(x, mu=256):
    return _sign(x) * _log1p(mu * _abs(x)) / _log1p(mu)
@@ -64,7 +72,11 @@ def read_pmfile(file):
    return timestamp_array
 
 def quantize_wavfile(file):
+   # Scipy and soundfile read wavefiles differently
    fs,A = wf.read(file)
+   A,fs = sf.read(file)
+   return A
+
    x_1 = (A / 32768.0).astype(np.float32)
    y_1 = mulaw_quantize(x_1,256)   
    return y_1
@@ -90,27 +102,38 @@ def make_pm_ccoeffs_wav_pm(file):
     frame_end = int(period_end * 1000)
     sample_start = int(period_start * 16000)
     sample_end = int(period_end * 16000)
+    
     #print period_start, period_end, frame_start, frame_end, sample_start, sample_end
     #print len(ccoeffs), len(wav_quantized)
     #print fname + '-audio-' + str(i).zfill(4) + '.npy|' + fname + '-mel-' + str(i).zfill(4) + '.npy|' + str(sample_end-sample_start) +  '|N/A|0'    
 
     c = ccoeffs[frame_start: frame_end]
-    np.save("data/" + fname + '-mel-' + str(i).zfill(4) + '.npy', c)
+    np.save("data_new/" + fname + '-mel-' + str(i).zfill(4) + '.npy', c)
     w = wav_quantized[sample_start:sample_end]
-    np.save("data/" + fname + '-audio-' + str(i).zfill(4) + '.npy',w)
+    np.save("data_new/" + fname + '-audio-' + str(i).zfill(4) + '.npy',w)
     if len(w) == 160 and len(c) == 10:   
         g.write(fname + '-audio-' + str(i).zfill(4) + '.npy|' + fname + '-mel-' + str(i).zfill(4) + '.npy|' + str(sample_end-sample_start) +  '|N/A|0' + '\n')
         h.write(fname + ' ' + str(sample_end-sample_start) + ' ' + str(frame_end - frame_start) + '\n') 
+        if save_plot :
+          axes = plt.gca()
+          axes.set_ylim([-0.9,0.9])
+          plt.plot(w)
+          plt.savefig('data_new/' + fname + '-plot-' + str(i).zfill(4) + '_noquantization.png')
+          plt.close()
 
 pitchmark_files = sorted(os.listdir('../voices/cmu_us_slt/pm/'))
 g = open('train.txt','w')
 h = open('log.txt','w')
+l = 0
 for f in pitchmark_files:
- if f[0] == '.':
+ #print f, l
+ #if l < 2:
+  if f[0] == '.':
    continue
- else: 
+  else: 
    print "Processing ", f
    filename = '../voices/cmu_us_slt/pm/' + f
    make_pm_ccoeffs_wav_pm(filename)
+   l += 1
 g.close()
 h.close()
