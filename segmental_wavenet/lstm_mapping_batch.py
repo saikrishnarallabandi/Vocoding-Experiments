6ac01196ef9400232f8bbbb408d9d458c780e2a9
saikrishnarallabandi <srallaba@cs.cmu.edu>
2018-08-04 17:51:47 -0400

    Added basic LSTM implementation

---
+++ segmental_wavenet/lstm_mapping_batch.py
@@ -0,0 +1,165 @@
+import os, sys
+import numpy as np
+from keras.utils import to_categorical
+from collections import defaultdict
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import soundfile as sf
+from logger import Logger
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import time
+
+'''
+Resources:
+https://github.com/MagaliDrumare/How-to-learn-PyTorch-NN-CNN-RNN-LSTM/blob/master/09-RNN.ipynb
+'''
+
+ccoeffs_folder = 'data_1msec'
+wav_folder = 'data_1msec'
+train_file = 'data_1msec/train.txt'
+test_file = 'test.txt'
+
+print_flag = 0
+num_train = 300000
+batch_size = 1
+epochs = 100
+saveplot_flag = 1
+log_flag = 1
+
+class Net(torch.nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.rnn = nn.LSTM(66, 256, batch_first=True, bidirectional=True) # input_dim, hidden_dim
+        self.fc = nn.Linear(512, 160) # https://stackoverflow.com/questions/45022734/understanding-a-simple-lstm-pytorch
+
+    def forward(self,x):
+        if print_flag:
+          print("Shape input to RNN is ", x.shape) # [1,10,66]
+        out, (h, cell) = self.rnn(x) # (batch_size, seq, input_size)
+        h_hat = h.view( h.shape[1], h.shape[0]*h.shape[2])
+        if print_flag :
+          print("Shape output from  RNN is ", x.shape) # [1, 10, 256]
+          print("Shape of hidden from RNN is ", h.shape)
+          print("Shape of RNN cell: ", cell.shape)
+          print("Modified shape of RNN hidden: ", h_hat.shape)
+        return F.relu(self.fc(h_hat)) #.tanh()
+
+
+class arctic_dataset(Dataset):
+      def __init__(self, ccoeffs_folder, wav_folder, train_file):
+        self.train_file = train_file
+        self.ccoeffs_folder = ccoeffs_folder
+        self.wav_folder = wav_folder
+        self.ccoeffs_array = []
+        self.wav_array= []
+        f = open(self.train_file)
+        ctr  = 0
+        for line in f: # arctic_a0001-audio-0000.npy|arctic_a0001-mel-0000.npy|160|N/A|0
+         if ctr < num_train:
+           ctr += 1
+           line = line.split('\n')[0].split('|')
+           wav_fname = line[0]
+           ccoeffs_fname = line[1]
+
+           ccoeffs = np.load(ccoeffs_folder + '/' + ccoeffs_fname)
+           self.ccoeffs_array.append(ccoeffs)
+
+           wav = np.load(wav_folder + '/' + wav_fname)
+           self.wav_array.append(wav)
+
+           if ctr % 1000 == 1:
+               print("Read ", len(self.ccoeffs_array), " sentences")
+
+
+      def __getitem__(self, index):
+           return self.ccoeffs_array[index], self.wav_array[index]
+
+
+      def __len__(self):
+           return len(self.ccoeffs_array) 
+
+def test(epoch):
+  test_array = []
+  for (a,b) in test_loader:
+     A = Variable(a).cuda()
+     B = Variable(b.unsqueeze_(0)).cuda()
+     B_out = net(A).squeeze(0).cpu().detach().numpy()
+     #print("Shape of B out: ", B_out.shape)
+     for w in B_out.T:
+       test_array.append(w)
+     #print("Shape of test array:", len(test_array))
+     w = np.array(test_array) * 1.0
+     sf.write('test_epoch' + str(epoch).zfill(3) + '.wav', np.asarray(w), 16000,format='wav',subtype="PCM_16")    
+     #print("Saved the file ", 'test_epoch' + str(epoch).zfill(3) + '.wav')
+     if epoch % 10 == 1:
+        saveplot_flag = 1
+     else:
+        saveplot_flag = 0
+
+     if saveplot_flag :
+          axes = plt.gca()
+          axes.set_ylim([-0.9,0.9])
+          plt.plot(w)
+          plt.savefig(str(epoch).zfill(3) + '-plot_noquantization.png')
+          plt.close()
+
+wks_train = arctic_dataset(ccoeffs_folder, wav_folder, train_file)
+train_loader = DataLoader(wks_train,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=4
+                         )
+wks_test = arctic_dataset(ccoeffs_folder, wav_folder, test_file)
+test_loader = DataLoader(wks_test,
+                          batch_size=1,
+                          shuffle=False,
+                          num_workers=4
+                         )
+
+
+net = Net()
+net.double().cuda()
+mse_loss = nn.MSELoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
+logger = Logger('./logs') 
+
+time_now = time.time()
+
+def main():
+ updates = 0
+ for epoch in range(epochs):
+  epoch_loss = 0
+  for i, (a,b) in enumerate(train_loader):
+     updates += 1
+     if print_flag:
+        print ("The shape of input is ", a.shape, b.shape, " done?")
+     A = Variable(a).cuda()
+     B = Variable(b.unsqueeze_(-1)).cuda()
+     B_out = net(A)
+     B_out.unsqueeze_(-1)
+     if print_flag:
+        print("The shape of output is ", B_out.shape)
+     loss = mse_loss(B_out, B)
+     epoch_loss += loss.cpu().data[0].numpy()
+     optimizer.zero_grad()
+     loss.backward()       
+     optimizer.step()
+     if log_flag:  
+        logger.scalar_summary('TrainLoss', epoch_loss * 1.0 / ((i+1)* batch_size) , updates)
+     if i % 1000 == 1:
+         print ("   Loss after batch ", i, " : ", epoch_loss/ ((i+1)* batch_size) * 1.0)
+  print ("Loss after epoch ", epoch, " : ", epoch_loss * 1.0 / ((i+1)*batch_size))  
+  time_current = time.time()
+  print("    Average Time taken per epoch: ", (time_current - time_now) * 1.0 / (1+epoch))
+  test(epoch)         
+  print('\n')
+
+main()
