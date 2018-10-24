280a22bd7bc682ab777c3ddab002c2713e3580d2
Sai Krishna <srallaba@cs.cmu.edu>
2018-10-23 20:58:11 -0400

    Update setup.sh

--- segmental_wavenet/classification/setup.sh
+++ segmental_wavenet/classification/setup.sh
@@ -4,17 +4,26 @@ cd feats
 wget http://tts.speech.cs.cmu.edu/rsk/tts_stuff/kitchen/segmental-wavenet-experiments/data/feats_slt_1msec_25June2018.tar.gz
 tar xvzf feats_slt_1msec_25June2018.tar.gz
 
-# Download pitchmark files
+# Download pitchmarks and wave files
 cd ../
 mkdir -p voices/cmu_us_slt
 cd voices/cmu_us_slt
 wget http://tts.speech.cs.cmu.edu/rsk/tts_stuff/kitchen/segmental-wavenet-experiments/data/pm_slt_26June2018.tar.gz
 tar xvzf pm_slt_26June2018.tar.gz
-
-# Download wav files
 wget http://festvox.org/cmu_arctic/packed/cmu_us_slt_arctic.tar.bz2
 tar xvjf cmu_us_slt_arctic.tar.bz2
 mv cmu_us_slt_arctic/wav .
+cd ../
+
+# If we dont have pitch marks, make them
+cd ../
+mkdir -p voices/cmu_us_slt
+cd voices/cmu_us_slt
+wget http://festvox.org/cmu_arctic/packed/cmu_us_slt_arctic.tar.bz2
+tar xvjf cmu_us_slt_arctic.tar.bz2
+mv cmu_us_slt_arctic/wav .
+./bin/make_pm_wave wav/* # If female voice, we need to change the arguments
+./bin/make_pm_fix pm/*
 cd ../
 
 # Clone the repo
@@ -24,4 +33,7 @@ git clone https://github.com/saikrishnarallabandi/Vocoding-Experiments
 cd ..
 
 # Get into the kitchen
-mkdir -p kitchen
+mkdir -p kitchen_slt
+# copy 02_wav_*, test_synth* initial_map* util.py model.py
+# Set paths and folder names correctly
+# Run 
